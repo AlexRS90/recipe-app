a23cccaa70fa2d4355982b6a27081ee96bbbdce7
AlexRS90 <aramosbengy@gmail.com>
2022-03-07 17:38:11 -0600

    Add navbar to logout of the session

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+  devise_scope :user do
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
+  
   root "home#index"
 end
