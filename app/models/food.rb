2b53477dbe0d00edb7af347241ba7de221b926ff
codepanther <pezechigozirim@gmail.com>
2022-03-09 23:53:22 +0100

    Create Inventory model and migration
    Create InventoryFood model for a many-to-many relationship with the Food and Inventory

--- app/models/food.rb
+++ app/models/food.rb
@@ -2,6 +2,8 @@ class Food < ApplicationRecord
   belongs_to :user
   has_many :recipe_foods
   has_many :recipes, through: :recipe_foods
+  has_many :inventory_foods
+  has_many :inventories, through: :inventory_foods
 
   validates :name, presence: { message: 'must not be empty' }
   validates :measurement_unit, presence: { message: 'must not be empty' }
