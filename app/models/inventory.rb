ffd8c7acacf3a83a87de402a82a94ecbaefc0169
codepanther <pezechigozirim@gmail.com>
2022-03-11 18:39:02 +0100

    Add clause to cascade on delete

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -1,6 +1,6 @@
 class Inventory < ApplicationRecord
   belongs_to :user
-  has_many :inventory_foods
+  has_many :inventory_foods, dependent: :delete_all
   has_many :foods, through: :inventory_foods
 
   validates :name, presence: { message: 'must not be empty' }
